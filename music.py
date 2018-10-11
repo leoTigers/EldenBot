9ba1f6ef9640037aadb3e398b3eb692f1fcb3a15
Etienne BERTRAND <etienne.b@idiese.com>
2018-10-11 14:25:14 +0200

    Added pause/resume function

--- music.py
+++ music.py
@@ -16,10 +16,29 @@ class CmdMusic:
             return
         if args[0] == "disconnect":
             if voice:
-                await self.disconnect()
+                voice.stop()
+                await voice.disconnect()
                 await message.channel.send("Client déconnecté")
+                voice = None
             else:
                 await message.channel.send("Le client est déjà déconnecté")
+        elif args[0] == "pause":
+            if not voice:
+                await message.channel.send("le client n'est pas connecté")
+            elif voice.is_paused():
+                await message.channel.send("déjà en pause")
+            elif not voice.is_playing():
+                await message.channel.send("aucune musique en cours")
+            else:
+                voice.pause()
+                await message.channel.send("mise en pause ...")
+        elif args[0] == "resume":
+            if not voice:
+                await message.channel.send("le client n'est pas connecté")
+            elif not voice.is_paused():
+                await message.channel.send("la pause n'est pas activé")
+            else:
+                voice.resume()
         else:
             if not voice or not voice.is_connected():
                 channel = get_client_channel(message.guild, member)
